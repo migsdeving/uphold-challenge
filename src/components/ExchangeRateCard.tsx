e0c7606c40da12bc8466fdf41f684703ddf4781c
Migs <miguelmigs24@gmail.com>
2023-12-01 19:53:59 +0000

    rerender improvements on the currency image

--- src/components/ExchangeRateCard.tsx
+++ src/components/ExchangeRateCard.tsx
@@ -1,3 +1,5 @@
+import { useCallback, useState } from "react";
+
 interface ExchangeRateCardProps {
   ticker: string;
   bid: number;
@@ -11,17 +13,34 @@ export const ExchangeRateCard = ({
   ask,
   currencyAmount,
 }: ExchangeRateCardProps) => {
+  const [isImageLoading, setIsImageLoading] = useState(true);
+  const handleImageError = useCallback(
+    ({ currentTarget }: React.SyntheticEvent<HTMLImageElement, Event>) => {
+      currentTarget.onerror = null; // prevents looping
+      currentTarget.src = "/assets/Crypto.png";
+      setIsImageLoading(false);
+    },
+    []
+  );
+
   return (
     <div className="flex justify-between items-center m-1">
       <div>{currencyAmount * ask}</div>
       <div className="flex justify-between items-center w-28 ">
+        {isImageLoading && (
+          <div className="animate-pulse flex space-x-4">
+            <div className="rounded-full bg-slate-700 h-10 w-10"></div>
+          </div>
+        )}
         <img
+          className={`${isImageLoading ? "hidden" : "block"}`}
           src={`/assets/${ticker}.png`}
-          onError={({ currentTarget }) => {
-            currentTarget.onerror = null; // prevents looping
-            currentTarget.src = "/assets/Crypto.png";
+          onError={handleImageError}
+          onLoad={() => {
+            setIsImageLoading(false);
           }}
         />
+
         <div className="ml-2">{ticker}</div>
       </div>
     </div>
