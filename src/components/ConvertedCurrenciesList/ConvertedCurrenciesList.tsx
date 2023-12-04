eca62c551669d271abe59f9df0d325f35cb7aaa0
Migs <miguelmigs24@gmail.com>
2023-12-04 00:56:04 +0000

    loading state for currency select change

--- src/components/ConvertedCurrenciesList/ConvertedCurrenciesList.tsx
+++ src/components/ConvertedCurrenciesList/ConvertedCurrenciesList.tsx
@@ -1,9 +1,11 @@
 import SDK from "@uphold/uphold-sdk-javascript";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Rate, setConversionRates } from "../../slices/conversionRate";
+import { SupportedCurrency } from "../../slices/supportedCurrencies";
 import { RootState } from "../../store";
 import { ExchangeRateCard } from "../ExchangeRateCard/ExchangeRateCard";
+import { ExchangeRateCardSkeleton } from "../ExchangeRateCard/ExchangeRateCardSkeleton";
 
 const sdk = new SDK({
   baseUrl: "http://api-sandbox.uphold.com",
@@ -28,7 +30,22 @@ export const ConvertedCurrenciesList = () => {
 
   const dispatch = useDispatch();
 
+  const [isLoading, setIsLoading] = useState(false);
+
+  const getCurrencyRate = (currency: SupportedCurrency) => {
+    const exchangePairHifen = `${currency.code}-${selectedCurrency.code}`;
+    const exchangePairSecondConcat = `${currency.code}${selectedCurrency.code}`;
+
+    const rate = conversionRates?.find(
+      (rate) =>
+        rate.pair === exchangePairHifen ||
+        rate.pair === exchangePairSecondConcat
+    );
+    return rate;
+  };
+
   const fetchRates = async () => {
+    if (!conversionRates) setIsLoading(true);
     const tickers: Rate[] = await sdk.getTicker(selectedCurrency.code);
     const filteredTickers = tickers.filter(
       (ticker) => ticker.currency === selectedCurrency.code
@@ -36,6 +53,7 @@ export const ConvertedCurrenciesList = () => {
     dispatch(
       setConversionRates({ key: selectedCurrency.code, data: filteredTickers })
     );
+    setIsLoading(false);
   };
 
   useEffect(() => {
@@ -44,17 +62,12 @@ export const ConvertedCurrenciesList = () => {
 
   return (
     <div className="flex justify-center mt-10 p-3 w-full">
-      {supportedCurrencies.length > 0 && !!currencyAmount ? (
+      {isLoading ? (
+        <ExchangeRateCardSkeleton />
+      ) : supportedCurrencies.length > 0 && !!currencyAmount ? (
         <div className="flex flex-col h-[70vh] w-full overflow-auto no-scrollbar">
           {supportedCurrencies.map((currency, index) => {
-            const exchangePairHifen = `${currency.code}-${selectedCurrency.code}`;
-            const exchangePairSecondConcat = `${currency.code}${selectedCurrency.code}`;
-
-            const rate = conversionRates?.find(
-              (rate) =>
-                rate.pair === exchangePairHifen ||
-                rate.pair === exchangePairSecondConcat
-            );
+            const rate = getCurrencyRate(currency);
             return rate ? (
               <ExchangeRateCard key={index} rate={rate} currency={currency} />
             ) : (
